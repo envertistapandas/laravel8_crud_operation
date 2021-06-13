ecce1d7daafa27b4efc1e639538c2be2c7442882
Tapan Das <tapandas025@gmail.com>
2021-06-13 10:54:55 +0530

    crud_with_image_done

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Laravel-8 Crud Operation With Image Upload</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
